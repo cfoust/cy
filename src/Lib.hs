e383ebfad3cf7e873528893af6f642e1f029eef1
Caleb Foust <cfoust@sqweebloid.com>
2020-07-18 09:31:59 -0700

    Basic bash run example

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,18 @@ module Lib
     ( someFunc
     ) where
 
+import System.Posix.Pty
+import System.Process
+import System.Exit
+import qualified Data.ByteString.Char8 as C
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  (pty, handle) <- spawnWithPty (Just [("SHELL", "bash")]) True "bash" ["--help"] (20, 10)
+  threadWaitReadPty pty
+  output <- readPty pty
+  exit <- waitForProcess handle
+  case exit of
+    ExitSuccess -> putStrLn "ok"
+    ExitFailure n -> putStrLn "failed"
+  C.putStrLn output
