497bd72fe500db8460662c34a6c6c10c2159fbb5
Caleb Foust <cfoust@sqweebloid.com>
2020-07-18 10:01:04 -0700

    ok this is cool

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,19 +1,31 @@
 module Lib
-    ( someFunc
-    ) where
+  ( proxyShell
+  )
+where
 
-import System.Posix.Pty
-import System.Process
-import System.Exit
-import qualified Data.ByteString.Char8 as C
+import           System.Posix.Pty
+import           System.Process
+import           System.Exit
+import qualified Data.ByteString.Char8         as C
 
-someFunc :: IO ()
-someFunc = do
-  (pty, handle) <- spawnWithPty (Just [("SHELL", "bash")]) True "bash" ["--help"] (20, 10)
+-- Continue polling IO and piping stdin/stdout until the program exits.
+pipeIO :: (Pty, ProcessHandle) -> IO ()
+pipeIO (pty, handle) = do
   threadWaitReadPty pty
-  output <- readPty pty
-  exit <- waitForProcess handle
-  case exit of
-    ExitSuccess -> putStrLn "ok"
-    ExitFailure n -> putStrLn "failed"
-  C.putStrLn output
+  b <- readPty pty
+  C.putStrLn b
+  code <- getProcessExitCode handle
+  case code of
+    Nothing -> pipeIO (pty, handle)
+    Just n  -> return ()
+
+proxyShell :: IO ()
+proxyShell = do
+  (pty, handle) <- spawnWithPty
+    (Just [("SHELL", "/bin/bash"), ("HOME", "/home/caleb")])
+    True
+    "bash"
+    []
+    (20, 10)
+  writePty pty (C.pack "touch blah\n")
+  pipeIO (pty, handle)
