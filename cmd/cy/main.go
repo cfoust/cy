399ededf041833604dbfe9bd5fe517cdfb85e19c
Caleb Foust <cfoust@sqweebloid.com>
2023-05-09 07:40:36 -0400

    feat: add go skeleton

---
+++ cmd/cy/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339})
+	log.Info().Msgf("hello world")
+}
