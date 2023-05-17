2d12a51544cfe00fef629b5f431a61fb0e9ba20d
Caleb Foust <cfoust@sqweebloid.com>
2023-05-17 18:53:36 -0400

    feat: collect events and dump to stdout

--- cmd/cy/main.go
+++ cmd/cy/main.go
@@ -15,13 +15,44 @@ import (
 	"golang.org/x/term"
 )
 
-type Write struct {
-	Data  []byte
-	Stamp time.Time
+type EventType byte
+
+const (
+	EventTypeInput EventType = iota
+	EventTypeOutput
+	EventTypeResize
+)
+
+type EventData interface {
+	Type() EventType
 }
 
-func proxy(ctx context.Context, dst io.Writer, src io.Reader) (<-chan Write, error) {
-	writes := make(chan Write)
+type Write struct {
+	Bytes []byte
+}
+
+type InputEvent Write
+
+func (i InputEvent) Type() EventType { return EventTypeInput }
+
+type OutputEvent Write
+
+func (i OutputEvent) Type() EventType { return EventTypeOutput }
+
+type ResizeEvent struct {
+	Width  int
+	Height int
+}
+
+func (i ResizeEvent) Type() EventType { return EventTypeResize }
+
+type Event struct {
+	Stamp time.Time
+	Data  EventData
+}
+
+func proxy(ctx context.Context, dst io.Writer, src io.Reader) (<-chan []byte, error) {
+	writes := make(chan []byte)
 
 	go func() {
 		buffer := make([]byte, 4096)
@@ -31,14 +62,22 @@ func proxy(ctx context.Context, dst io.Writer, src io.Reader) (<-chan Write, err
 			}
 
 			numBytes, err := src.Read(buffer)
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				// TODO(cfoust): 05/17/23
+				log.Error().Err(err).Msg("could not read from stream")
+				return
+			}
 			if numBytes == 0 {
 				continue
 			}
 
-			log.Info().Msgf("%+v", string(buffer[:numBytes]))
-			if err != nil {
-				return
-			}
+			copied := make([]byte, numBytes)
+			copy(copied, buffer[:numBytes])
+			writes <- copied
+
 			dst.Write(buffer[:numBytes])
 		}
 	}()
@@ -46,7 +85,7 @@ func proxy(ctx context.Context, dst io.Writer, src io.Reader) (<-chan Write, err
 	return writes, nil
 }
 
-func record(ctx context.Context, command string) error {
+func proxyShell(ctx context.Context, command string, events chan Event) error {
 	shell := exec.Command(command)
 	ptmx, err := pty.Start(shell)
 	if err != nil {
@@ -63,6 +102,18 @@ func record(ctx context.Context, command string) error {
 			case <-ctx.Done():
 				return
 			case <-ch:
+				width, height, err := term.GetSize(int(os.Stdin.Fd()))
+				if err != nil {
+					log.Error().Err(err).Msg("failed to get terminal dimensions")
+					return
+				}
+				events <- Event{
+					Stamp: time.Now(),
+					Data: ResizeEvent{
+						Width:  width,
+						Height: height,
+					},
+				}
 				pty.InheritSize(os.Stdin, ptmx)
 			}
 		}
@@ -73,27 +124,94 @@ func record(ctx context.Context, command string) error {
 	// Change to raw mode
 	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
-		panic(err)
-	}
-	defer func() { _ = term.Restore(int(os.Stdin.Fd()), oldState) }() // Best effort.
-
-	proxy(ctx, ptmx, os.Stdin)
-	proxy(ctx, os.Stdout, ptmx)
-
-	err = shell.Wait()
-	if err != nil {
 		return err
 	}
+	defer func() { _ = term.Restore(int(os.Stdin.Fd()), oldState) }()
 
-	return nil
+	go func() {
+		writes, err := proxy(ctx, ptmx, os.Stdin)
+		if err != nil {
+			log.Error().Err(err).Msg("failed to read user input")
+			return
+		}
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case write := <-writes:
+				events <- Event{
+					Stamp: time.Now(),
+					Data: InputEvent{
+						Bytes: write,
+					},
+				}
+			}
+		}
+	}()
+
+	go func() {
+		reads, err := proxy(ctx, os.Stdout, ptmx)
+		if err != nil {
+			log.Error().Err(err).Msg("failed to read shell output")
+			return
+		}
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case read := <-reads:
+				events <- Event{
+					Stamp: time.Now(),
+					Data: OutputEvent{
+						Bytes: read,
+					},
+				}
+			}
+		}
+	}()
+
+	return shell.Wait()
+}
+
+func record(ctx context.Context, command string) (<-chan Event, <-chan error) {
+	events := make(chan Event)
+	done := make(chan error)
+
+	go func() {
+		done <- proxyShell(ctx, command, events)
+	}()
+
+	return events, done
 }
 
 func main() {
 	consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
 	log.Logger = log.Output(consoleWriter)
 
-	err := record(context.Background(), "/bin/bash")
+	events, done := record(context.Background(), "/bin/bash")
+
+	eventList := make([]Event, 0)
+	go func() {
+		for {
+			select {
+			case <-context.Background().Done():
+				return
+			case event := <-events:
+				eventList = append(eventList, event)
+			}
+		}
+	}()
+
+	err := <-done
 	if err != nil {
-		panic(err)
+		log.Panic().Err(err).Msg("shell exited with error")
 	}
+
+	for _, event := range eventList {
+		log.Info().Msgf("%+v", event)
+	}
+
+	log.Info().Msgf("done")
 }
