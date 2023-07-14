abc53a326679f8bb0cd723707b512c5db202caa5
Caleb Foust <cfoust@sqweebloid.com>
2023-07-14 18:10:28 -0400

    feat: turn session into an App

--- pkg/app/recorder.go
+++ pkg/app/recorder.go
@@ -1,9 +1,8 @@
-package session
+package app
 
 import (
 	"time"
 
-	"github.com/cfoust/cy/pkg/util"
 	"github.com/sasha-s/go-deadlock"
 )
 
@@ -32,8 +31,8 @@ type OutputEvent Write
 func (i OutputEvent) Type() EventType { return EventTypeOutput }
 
 type ResizeEvent struct {
-	Width  int
-	Height int
+	Columns int
+	Rows    int
 }
 
 func (i ResizeEvent) Type() EventType { return EventTypeResize }
@@ -43,18 +42,19 @@ type Event struct {
 	Data  EventData
 }
 
-type Session struct {
+type Recorder struct {
 	events []Event
 	mutex  deadlock.RWMutex
+	app    App
 }
 
-func New() *Session {
-	return &Session{
-		events: make([]Event, 0),
-	}
+var _ App = (*Recorder)(nil)
+
+func New() *Recorder {
+	return &Recorder{}
 }
 
-func (s *Session) store(data EventData) {
+func (s *Recorder) store(data EventData) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 	s.events = append(s.events, Event{
@@ -63,26 +63,42 @@ func (s *Session) store(data EventData) {
 	})
 }
 
-func (s *Session) Events() []Event {
+func (s *Recorder) Events() []Event {
 	s.mutex.Lock()
 	events := s.events
 	s.mutex.Unlock()
 	return events
 }
 
-func (s *Session) Input(data []byte) {
+func (s *Recorder) Write(data []byte) (n int, err error) {
 	s.store(InputEvent{Bytes: data})
+	return s.app.Write(data)
 }
 
-func (s *Session) Output(data []byte) {
+func (s *Recorder) Read(p []byte) (n int, err error) {
+	n, err = s.app.Read(p)
+	if err != nil {
+		return n, err
+	}
+
+	data := make([]byte, n)
+	copy(data, p)
 	s.store(OutputEvent{Bytes: data})
+	return
 }
 
-func (s *Session) Resize(width, height int) {
+func (s *Recorder) Resize(size Size) error {
 	s.store(ResizeEvent{
-		Width:  width,
-		Height: height,
+		Columns: size.Columns,
+		Rows:    size.Rows,
 	})
+
+	return nil
 }
 
-var _ util.Resizable = (*Session)(nil)
+func NewRecorder(app App) *Recorder {
+	return &Recorder{
+		events: make([]Event, 0),
+		app:    app,
+	}
+}
