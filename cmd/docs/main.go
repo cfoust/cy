3ac367966b6c82c09698c055102c35eff1c57376
Caleb Foust <cfoust@sqweebloid.com>
2023-11-26 12:07:22 -0500

    feat: automatic generation for API

--- cmd/docs/main.go
+++ cmd/docs/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"sort"
+	"strings"
 
 	"github.com/cfoust/cy/pkg/cy"
 	"github.com/cfoust/cy/pkg/janet"
@@ -18,6 +20,11 @@ var GEN_API string
 var CLI struct {
 	API struct {
 	} `cmd:"" help:"Generate Markdown for the full API."`
+}
+
+type Symbol struct {
+	Name, Docstring, Link string
+	Macro                 bool
 }
 
 func main() {
@@ -45,14 +52,68 @@ func main() {
 			panic(err)
 		}
 
+		symbols := make([]Symbol, 0)
 		cy.Callback("cy/doc", func(name string, docstring string, link string, macro bool) {
-			fmt.Printf("%s %s %t\n", name, docstring, macro)
+			symbols = append(symbols, Symbol{
+				Name:      name,
+				Docstring: docstring,
+				Link:      link,
+				Macro:     macro,
+			})
 		})
 
 		err = cy.Execute(ctx, GEN_API)
 		if err != nil {
 			panic(err)
 		}
+
+		sort.SliceStable(symbols, func(i, j int) bool {
+			return symbols[i].Name < symbols[j].Name
+		})
+
+		var output string
+
+		// Generate the table of contents
+		output += "## Index\n\n"
+		for _, symbol := range symbols {
+			header := strings.Map(func(r rune) rune {
+				if r == '/' {
+					return -1
+				}
+
+				return r
+			}, symbol.Name)
+
+			output += fmt.Sprintf("[%s](#%s) ", symbol.Name, header)
+		}
+
+		output += "\n\n---\n"
+
+		for _, symbol := range symbols {
+			_type := "function"
+
+			if symbol.Macro {
+				_type = "macro"
+			}
+
+			source := ""
+			if len(symbol.Link) > 0 {
+				source = fmt.Sprintf("[source](%s)\n", symbol.Link)
+			}
+
+			output += fmt.Sprintf(`
+#### %s
+
+_%s_
+
+%s
+%s
+
+---
+`, symbol.Name, _type, source, symbol.Docstring)
+		}
+
+		fmt.Println(output)
 	default:
 		panic(ctx.Command())
 	}
