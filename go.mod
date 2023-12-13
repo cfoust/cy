da97d708d2b9f282af5a66e17f709cf554e9b39b
Caleb Foust <cfoust@sqweebloid.com>
2023-12-13 05:53:41 -0500

    feat: upgrade to go 1.21

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/cfoust/cy
 
-go 1.19
+go 1.21
 
 require (
 	github.com/alecthomas/chroma v0.10.0 // indirect
--- pkg/version/module.go
+++ pkg/version/module.go
@@ -3,7 +3,7 @@ package version
 // These should be set via go build -ldflags -X 'xxxx'.
 var (
 	Version   = "development"
-	GoVersion = "1.19"
+	GoVersion = "1.21"
 	GitCommit = "unknown"
 	BuildTime = "unknown"
 )
