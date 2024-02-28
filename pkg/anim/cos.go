176e480b30e8c1a214b1a92f83adb1561ff9111c
Caleb Foust <cfoust@sqweebloid.com>
2024-02-28 20:21:26 +0800

    feat: cos animation

---
+++ pkg/anim/cos.go
@@ -0,0 +1,43 @@
+package anim
+
+import (
+	"math"
+	"time"
+
+	"github.com/cfoust/cy/pkg/geom/image"
+)
+
+type Cos struct {
+	in  image.Image
+	out image.Image
+}
+
+var _ Animation = (*Cos)(nil)
+
+func (cos *Cos) Init(start image.Image) {
+	cos.in = start.Clone()
+	cos.out = start
+}
+
+func (cos *Cos) Update(delta time.Duration) image.Image {
+	elapsed := delta.Seconds()
+	size := cos.out.Size()
+
+	var factor float64
+	var offset int
+	for col := 0; col < size.C; col++ {
+		factor = ((math.Cos((float64(col)/float64(size.C))*math.Pi*4+elapsed) + 1) / 8) * float64(size.R)
+		offset = int(factor)
+		for row := 0; row < size.R; row++ {
+			cos.out[(row+offset)%size.R][col] = cos.in[row][col]
+		}
+	}
+
+	return cos.out
+}
+
+func init() {
+	registerAnimation("cos", func() Animation {
+		return &Cos{}
+	})
+}
