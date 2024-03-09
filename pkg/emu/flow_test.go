18ac77d8e5d994ad6e12b7f2bd82ec6ef83cbae6
Caleb Foust <cfoust@sqweebloid.com>
2024-03-09 14:05:35 +0800

    feat: tests for Flow()

--- pkg/emu/flow_test.go
+++ pkg/emu/flow_test.go
@@ -3,6 +3,8 @@ package emu
 import (
 	"testing"
 
+	"github.com/cfoust/cy/pkg/geom"
+
 	"github.com/stretchr/testify/require"
 )
 
@@ -65,21 +67,180 @@ func TestBreakLine(t *testing.T) {
 
 func TestFlowLines(t *testing.T) {
 	term := New()
-	term.Resize(2, 4)
+	term.Resize(4, 2)
 	term.Write([]byte(LineFeedMode))
 	term.Write([]byte("foo\nfoobar\nbaz"))
-	// should be:
+	// flow should be:
 	// 0: foo
 	// 1: foob
-	// ---
+	// --- the screen starts
 	// 1: ar
 	// 2: baz
 
-	//lines, ok := term.Flow(
-	//geom.Vec2{R: 2, C: 4},
-	//geom.Vec2{R: 1},
-	//-2,
-	//)
-	//t.Logf("%+v %+v", lines, ok)
-	//t.Fail()
+	require.Equal(
+		t,
+		geom.Vec2{
+			R: 1,
+			C: 4,
+		},
+		term.Root(),
+	)
+
+	// Remove anything in lines that can mess with comparisons
+	cleanLines := func(lines []FlowLine) {
+		for _, line := range lines {
+			for i := range line.Chars {
+				line.Chars[i].Write = 0
+				line.Chars[i].Mode = 0
+			}
+		}
+	}
+
+	// 1. Just check that the screen is correct
+	{
+		lines, ok := term.Flow(
+			geom.Vec2{
+				R: 2,
+				C: 4,
+			},
+			term.Root(),
+			2,
+		)
+
+		cleanLines(lines)
+
+		require.True(t, ok)
+		require.Equal(t,
+			[]FlowLine{
+				{
+					R:  1,
+					C0: 4,
+					C1: 6,
+					Chars: makeLine(
+						"ar",
+					),
+				},
+				{
+					R:  2,
+					C0: 0,
+					C1: 3,
+					Chars: makeLine(
+						"baz",
+					),
+				},
+			},
+			lines,
+		)
+	}
+
+	// 2. Check that negative counts work correctly
+	{
+		lines, ok := term.Flow(
+			geom.Vec2{
+				R: 2,
+				C: 4,
+			},
+			term.Root(),
+			-2,
+		)
+
+		cleanLines(lines)
+
+		require.True(t, ok)
+		require.Equal(t,
+			[]FlowLine{
+				{
+					R:  0,
+					C0: 0,
+					C1: 3,
+					Chars: makeLine(
+						"foo",
+					),
+				},
+				{
+					R:  1,
+					C0: 0,
+					C1: 4,
+					Chars: makeLine(
+						"foob",
+					),
+				},
+			},
+			lines,
+		)
+	}
+
+	// 3. Do something really weird
+	{
+		lines, ok := term.Flow(
+			geom.Vec2{C: 2},
+			geom.Vec2{},
+			10,
+		)
+
+		cleanLines(lines)
+
+		require.True(t, ok)
+		require.Equal(t,
+			[]FlowLine{
+				{
+					R:  0,
+					C0: 0,
+					C1: 2,
+					Chars: makeLine(
+						"fo",
+					),
+				},
+				{
+					R:  0,
+					C0: 2,
+					C1: 3,
+					Chars: makeLine(
+						"o",
+					),
+				},
+				{
+					R:  1,
+					C0: 0,
+					C1: 2,
+					Chars: makeLine(
+						"fo",
+					),
+				},
+				{
+					R:  1,
+					C0: 2,
+					C1: 4,
+					Chars: makeLine(
+						"ob",
+					),
+				},
+				{
+					R:  1,
+					C0: 4,
+					C1: 6,
+					Chars: makeLine(
+						"ar",
+					),
+				},
+				{
+					R:  2,
+					C0: 0,
+					C1: 2,
+					Chars: makeLine(
+						"ba",
+					),
+				},
+				{
+					R:  2,
+					C0: 2,
+					C1: 3,
+					Chars: makeLine(
+						"z",
+					),
+				},
+			},
+			lines,
+		)
+	}
 }
