b6d8958ffcc84a581dec90e522a7d9ea9123cb8c
Caleb Foust <cfoust@sqweebloid.com>
2024-07-27 08:21:06 +0800

    fix: rebase goof

--- pkg/cy/api/viewport.go
+++ pkg/cy/api/viewport.go
@@ -1,11 +1,27 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/cfoust/cy/pkg/anim"
 	"github.com/cfoust/cy/pkg/frames"
 )
 
 type ViewportModule struct {
+}
+
+func (c *ViewportModule) SetFrame(context interface{}, name string) error {
+	client, err := getClient(context)
+	if err != nil {
+		return err
+	}
+
+	frame, ok := frames.Frames[name]
+	if !ok {
+		return fmt.Errorf("could not find frame '%s'", name)
+	}
+	client.Frame().Set(frame)
+	return nil
 }
 
 func (c *ViewportModule) GetFrames(context interface{}) []string {
