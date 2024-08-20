5858ef38a3f0ac0cd277f9025531b5ea3acbd5e8
Caleb Foust <cfoust@sqweebloid.com>
2024-08-20 09:17:36 +0800

    chore: formatting

--- pkg/cy/constants.go
+++ pkg/cy/constants.go
@@ -9,5 +9,5 @@ var (
 	CONTEXT_REGEX = regexp.MustCompile("^(?P<socket>\\w+):(?P<id>\\d+)$")
 	// Regex used for validating socket names, which must be alphanumeric and not
 	// contain spaces.
-	SOCKET_REGEX  = regexp.MustCompile("^(\\w+)$")
+	SOCKET_REGEX = regexp.MustCompile("^(\\w+)$")
 )
