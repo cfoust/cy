7d70ef3da11031c27e068f4080258256704b27a7
Caleb Foust <cfoust@sqweebloid.com>
2024-09-13 08:56:39 +0800

    feat: normalize borg path

--- pkg/cmd/store.go
+++ pkg/cmd/store.go
@@ -50,19 +50,28 @@ func (s *Store) OpenDatabases(dirs []string) error {
 
 // QueryCommands queries commands across all open command databases.
 func (s *Store) QueryCommands(ctx context.Context) ([]CommandEvent, error) {
-	dbs := make([]*DB, 0, len(s.dbs))
+	dbs := make(map[string]*DB)
 	s.RLock()
-	for _, db := range s.dbs {
-		dbs = append(dbs, db)
+	for path, db := range s.dbs {
+		dbs[path] = db
 	}
 	s.RUnlock()
 
 	allCommands := make([]CommandEvent, 0)
-	for _, db := range dbs {
+	for dbPath, db := range dbs {
 		commands, err := db.ListCommands(ctx)
 		if err != nil {
 			return nil, err
 		}
+
+		// Make .borg path absolute
+		for _, command := range commands {
+			command.Borg = path.Join(
+				path.Dir(dbPath),
+				command.Borg,
+			)
+		}
+
 		allCommands = append(allCommands, commands...)
 	}
 
@@ -82,9 +91,10 @@ func (s *Store) SaveCommand(ctx context.Context, c CommandEvent) error {
 		return db.CreateCommand(ctx, c)
 	}
 
-	// TODO(cfoust): 09/13/24 normalize borg path
+	// Make .borg path relative to db
+	c.Borg = path.Base(c.Borg)
+
 	// TODO(cfoust): 09/12/24 create lock file before creating db
-	// TODO(cfoust): 09/12/24 index existing .borg files
 	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
 		db, err = Create(dbPath)
 		if err != nil {
