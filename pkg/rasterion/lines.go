5061d12f15479a089837db43404dcbf5e7e0fb47
Caleb Foust <cfoust@sqweebloid.com>
2025-08-07 18:16:57 -0700

    feat: cell intersections

--- pkg/rasterion/lines.go
+++ pkg/rasterion/lines.go
@@ -7,6 +7,14 @@ import (
 	gl "github.com/go-gl/mathgl/mgl32"
 )
 
+// clampVector clamps the components of a Vec2 to the given range.
+func clampVector(v gl.Vec2, low, high float32) gl.Vec2 {
+	return gl.Vec2{
+		gl.Clamp(v[0], low, high),
+		gl.Clamp(v[1], low, high),
+	}
+}
+
 // line draws a line on the bitmap using Bresenham's algorithm.
 func (c *Context) line(
 	s LineShader,
@@ -22,6 +30,8 @@ func (c *Context) line(
 		}
 		v0, w0, b0 = c.transformPoint(s, viewport, 0, 0, p0)
 		v1, w1, b1 = c.transformPoint(s, viewport, 0, 1, p1)
+		w0_2       = w0.Vec2()
+		w1_2       = w1.Vec2()
 	)
 
 	if b0 || b1 {
@@ -33,7 +43,7 @@ func (c *Context) line(
 			float32(size.C - 1),
 			float32(size.R - 1),
 		},
-	}.Intersections(w0.Vec2(), w1.Vec2())
+	}.Intersections(w0_2, w1_2)
 
 	// Exclude lines that are not on the screen
 	if !onScreen {
@@ -52,13 +62,17 @@ func (c *Context) line(
 	)
 
 	var (
-		line    = w1.Sub(w0)
-		length  = line.Len()
-		p       gl.Vec4
-		glyph   emu.Glyph
-		discard bool
-		t       float32
-		invZ    float32
+		line   = w1.Sub(w0)
+		length = line.Len()
+		p      gl.Vec4
+		cell   = Rect{
+			Size: gl.Vec2{1, 1},
+		}
+		glyph               emu.Glyph
+		discard, intersects bool
+		in0, in1            gl.Vec2
+		t                   float32
+		invZ                float32
 	)
 
 	line = line.Normalize()
@@ -84,6 +98,8 @@ func (c *Context) line(
 		// Center the point in the cell
 		p[0] = float32(x0) + 0.5
 		p[1] = float32(y0) + 0.5
+		cell.Pos[0] = float32(x0)
+		cell.Pos[1] = float32(y0)
 
 		// Calculate distance along line
 		t = gl.Clamp(
@@ -95,12 +111,43 @@ func (c *Context) line(
 		invZ = (1-t)*oneOverZ[0] + t*oneOverZ[1]
 		t = (1 - t) * oneOverZ[0] / invZ
 
+		// We provide the fragment shader with the intersections of the
+		// line we're drawing with the current cell expressed as two
+		// vectors in the coordinate frame of the cell. The components
+		// of these vectors are between [0, 1].
+		in0, in1, intersects, _ = cell.Intersections(w0_2, w1_2)
+
+		// Most of the time the line we're drawing intersects with the
+		// cell the Bresenham algorithm is drawing.
+		if intersects {
+			in0 = in0.Sub(cell.Pos)
+			in1 = in1.Sub(cell.Pos)
+		} else {
+			// But when it doesn't intersect, which can happen, we
+			// pretend that the cell is closer to the line than it
+			// actually is.
+			nearest := w1.
+				Sub(w0).
+				Mul(1 - t).
+				Add(w0).
+				Vec2()
+			// TODO(cfoust): 03/25/25 allow user to configure fuzziness
+			in0, in1, _, _ = Rect{
+				Pos:  nearest,
+				Size: gl.Vec2{1, 1},
+			}.Intersections(w0_2, w1_2)
+			in0 = in0.Sub(nearest)
+			in1 = in1.Sub(nearest)
+		}
+
+		in0 = clampVector(in0, 0, 1)
+		in1 = clampVector(in1, 0, 1)
+
 		glyph, discard = s.Fragment(
 			i0, i1,
-			gl.Vec3{}, gl.Vec3{},
+			in0.Vec3(1), in1.Vec3(1),
 			t,
 		)
-
 		if !discard {
 			image[y0][x0] = glyph
 		}
