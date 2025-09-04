d5873e41781b13b20ab81985133e0efa4e6333ae
Caleb Foust <cfoust@sqweebloid.com>
2025-09-04 07:29:29 -0700

    feat: integrate full list of key constants

--- pkg/keys/kitty.go
+++ pkg/keys/kitty.go
@@ -8,35 +8,172 @@ import (
 	"github.com/cfoust/cy/pkg/emu"
 )
 
-// Kitty protocol special keys using Unicode Private Use Area
 const (
-	KittyKeyEscape    = 0x1B
-	KittyKeyEnter     = 0x0D
-	KittyKeyTab       = 0x09
-	KittyKeyBackspace = 0x7F
-	KittyKeyInsert    = 0xE000 + 2
-	KittyKeyDelete    = 0xE000 + 3
-	KittyKeyHome      = 0xE000 + 1
-	KittyKeyEnd       = 0xE000 + 4
-	KittyKeyPageUp    = 0xE000 + 5
-	KittyKeyPageDown  = 0xE000 + 6
-	KittyKeyLeft      = 0xE000 + 68
-	KittyKeyUp        = 0xE000 + 65
-	KittyKeyRight     = 0xE000 + 67
-	KittyKeyDown      = 0xE000 + 66
-	KittyKeyF1        = 0xE000 + 112
-	KittyKeyF2        = 0xE000 + 113
-	KittyKeyF3        = 0xE000 + 114
-	KittyKeyF4        = 0xE000 + 115
-	KittyKeyF5        = 0xE000 + 116
-	KittyKeyF6        = 0xE000 + 117
-	KittyKeyF7        = 0xE000 + 118
-	KittyKeyF8        = 0xE000 + 119
-	KittyKeyF9        = 0xE000 + 120
-	KittyKeyF10       = 0xE000 + 121
-	KittyKeyF11       = 0xE000 + 122
-	KittyKeyF12       = 0xE000 + 123
+	// The protocol defines special codes for these
+	KittyKeyEscape    = 27
+	KittyKeyEnter     = 13
+	KittyKeyTab       = 9
+	KittyKeyBackspace = 127
+
+	// There are a bunch of keys that have irregular encodings; we use our
+	// own internal codes for these
+	/////////////////////////////////////////////////////////////////////
+	// Navigation Keys (using private use area for irregular CSI sequences)
+	KittyKeyHome     = 0xE000 + 1 // 1 H or 7 ~
+	KittyKeyInsert   = 0xE000 + 2
+	KittyKeyDelete   = 0xE000 + 3
+	KittyKeyEnd      = 0xE000 + 4 // 1 F or 8 ~
+	KittyKeyPageUp   = 0xE000 + 5
+	KittyKeyPageDown = 0xE000 + 6
+	KittyKeyUp       = 0xE000 + 65 // 1 A
+	KittyKeyDown     = 0xE000 + 66 // 1 B
+	KittyKeyRight    = 0xE000 + 67 // 1 C
+	KittyKeyLeft     = 0xE000 + 68 // 1 D
+
+	// Function Keys F1-F12 (using private use area for irregular CSI sequences)
+	KittyKeyF1  = 0xE000 + 112 // 1 P or 11 ~
+	KittyKeyF2  = 0xE000 + 113 // 1 Q or 12 ~
+	KittyKeyF3  = 0xE000 + 114 // 13 ~
+	KittyKeyF4  = 0xE000 + 115 // 1 S or 14 ~
+	KittyKeyF5  = 0xE000 + 116 // 15 ~
+	KittyKeyF6  = 0xE000 + 117 // 17 ~
+	KittyKeyF7  = 0xE000 + 118 // 18 ~
+	KittyKeyF8  = 0xE000 + 119 // 19 ~
+	KittyKeyF9  = 0xE000 + 120 // 20 ~
+	KittyKeyF10 = 0xE000 + 121 // 21 ~
+	KittyKeyF11 = 0xE000 + 122 // 23 ~
+	KittyKeyF12 = 0xE000 + 123 // 24 ~
+	/////////////////////////////////////////////////////////////////////
+
+	// The rest of the keys all have fixed codes
+	// Lock/System Keys
+	KittyCapsLock    = 57358
+	KittyScrollLock  = 57359
+	KittyNumLock     = 57360
+	KittyPrintScreen = 57361
+	KittyPause       = 57362
+	KittyMenu        = 57363
+
+	// Function Keys F13-F35 (standard u format)
+	KittyKeyF13 = 57376
+	KittyKeyF14 = 57377
+	KittyKeyF15 = 57378
+	KittyKeyF16 = 57379
+	KittyKeyF17 = 57380
+	KittyKeyF18 = 57381
+	KittyKeyF19 = 57382
+	KittyKeyF20 = 57383
+	KittyKeyF21 = 57384
+	KittyKeyF22 = 57385
+	KittyKeyF23 = 57386
+	KittyKeyF24 = 57387
+	KittyKeyF25 = 57388
+	KittyKeyF26 = 57389
+	KittyKeyF27 = 57390
+	KittyKeyF28 = 57391
+	KittyKeyF29 = 57392
+	KittyKeyF30 = 57393
+	KittyKeyF31 = 57394
+	KittyKeyF32 = 57395
+	KittyKeyF33 = 57396
+	KittyKeyF34 = 57397
+	KittyKeyF35 = 57398
+
+	// Keypad Keys
+	KittyKP0         = 57399
+	KittyKP1         = 57400
+	KittyKP2         = 57401
+	KittyKP3         = 57402
+	KittyKP4         = 57403
+	KittyKP5         = 57404
+	KittyKP6         = 57405
+	KittyKP7         = 57406
+	KittyKP8         = 57407
+	KittyKP9         = 57408
+	KittyKPDecimal   = 57409
+	KittyKPDivide    = 57410
+	KittyKPMultiply  = 57411
+	KittyKPSubtract  = 57412
+	KittyKPAdd       = 57413
+	KittyKPEnter     = 57414
+	KittyKPEqual     = 57415
+	KittyKPSeparator = 57416
+	KittyKPLeft      = 57417
+	KittyKPRight     = 57418
+	KittyKPUp        = 57419
+	KittyKPDown      = 57420
+	KittyKPPageUp    = 57421
+	KittyKPPageDown  = 57422
+	KittyKPHome      = 57423
+	KittyKPEnd       = 57424
+	KittyKPInsert    = 57425
+	KittyKPDelete    = 57426
+	KittyKPBegin     = 57427
+
+	// Media Keys
+	KittyMediaPlay        = 57428
+	KittyMediaPause       = 57429
+	KittyMediaPlayPause   = 57430
+	KittyMediaReverse     = 57431
+	KittyMediaStop        = 57432
+	KittyMediaFastForward = 57433
+	KittyMediaRewind      = 57434
+	KittyMediaTrackNext   = 57435
+	KittyMediaTrackPrev   = 57436
+	KittyMediaRecord      = 57437
+	KittyLowerVolume      = 57438
+	KittyRaiseVolume      = 57439
+	KittyMuteVolume       = 57440
+
+	// Modifier Keys
+	KittyLeftShift    = 57441
+	KittyLeftControl  = 57442
+	KittyLeftAlt      = 57443
+	KittyLeftSuper    = 57444
+	KittyLeftHyper    = 57445
+	KittyLeftMeta     = 57446
+	KittyRightShift   = 57447
+	KittyRightControl = 57448
+	KittyRightAlt     = 57449
+	KittyRightSuper   = 57450
+	KittyRightHyper   = 57451
+	KittyRightMeta    = 57452
 )
+
+// irregularCSI maps key constants to their irregular CSI sequences
+// (i.e., those that don't follow the standard "ESC[{number}u" format)
+var irregularCSI = map[rune]string{
+	// Navigation keys with letter suffixes
+	KittyKeyLeft:  "1D",
+	KittyKeyRight: "1C",
+	KittyKeyUp:    "1A",
+	KittyKeyDown:  "1B",
+	KittyKeyHome:  "1H", // Alternative: "7~"
+	KittyKeyEnd:   "1F", // Alternative: "8~"
+	KittyKPBegin:  "1E", // Alternative: "57427 ~"
+
+	// Keys with ~ suffix
+	KittyKeyInsert:   "2~",
+	KittyKeyDelete:   "3~",
+	KittyKeyPageUp:   "5~",
+	KittyKeyPageDown: "6~",
+
+	// Function keys F1-F4 with letter suffixes
+	KittyKeyF1: "1P", // Alternative: "11~"
+	KittyKeyF2: "1Q", // Alternative: "12~"
+	KittyKeyF4: "1S", // Alternative: "14~"
+
+	// Function keys F3,F5-F12 with ~ suffix
+	KittyKeyF3:  "13~",
+	KittyKeyF5:  "15~",
+	KittyKeyF6:  "17~",
+	KittyKeyF7:  "18~",
+	KittyKeyF8:  "19~",
+	KittyKeyF9:  "20~",
+	KittyKeyF10: "21~",
+	KittyKeyF11: "23~",
+	KittyKeyF12: "24~",
+}
 
 // isKittySequence checks if the byte sequence might be a Kitty protocol sequence
 // Kitty sequences follow the pattern: ESC [ {keycode} [; {modifiers}] u
@@ -57,11 +194,6 @@ func isKittySequence(b []byte) bool {
 		return false
 	}
 
-	// Check if it's a keyboard protocol command (starts with >, <, or ?)
-	if len(b) > 2 && (b[2] == '>' || b[2] == '<' || b[2] == '?') {
-		return false
-	}
-
 	// Content between '[' and 'u' must start with a digit (keycode)
 	content := string(b[2 : len(b)-1])
 	if len(content) == 0 {
@@ -74,12 +206,12 @@ func isKittySequence(b []byte) bool {
 
 // kittySequence generates the Kitty protocol sequence for this key
 func (k Key) kittySequence(protocol emu.KeyProtocol) string {
-	if len(k.Runes) == 0 {
+	if k.Code == 0 {
 		return ""
 	}
 
 	var (
-		keycode   = k.Runes[0]
+		keycode   = k.Code
 		modifiers = int(k.Mod)
 		eventType = int(k.Type)
 		// Check if event types should be reported
@@ -91,15 +223,9 @@ func (k Key) kittySequence(protocol emu.KeyProtocol) string {
 	)
 
 	// Determine if we should include text in output
-	// Extract printable characters from Runes field for text support
 	var text string
 	if len(k.Text) > 0 {
 		text = k.Text
-	} else if len(k.Runes) > 0 {
-		printableRunes := onlyPrintable(k.Runes)
-		if len(printableRunes) > 0 {
-			text = string(printableRunes)
-		}
 	}
 	includeText := reportText && text != ""
 
@@ -147,8 +273,8 @@ func parseKittySequence(b []byte) (Key, int, error) {
 	}
 
 	key := Key{
-		Runes: []rune{rune(keycode)},
-		Type:  KeyEventPress, // Default to press event
+		Code: rune(keycode),
+		Type: KeyEventPress, // Default to press event
 	}
 
 	// Parse modifiers (optional)
@@ -190,7 +316,7 @@ func parseKittySequence(b []byte) (Key, int, error) {
 
 // GenerateKittyEnableSequence generates the escape sequence to enable Kitty protocol
 func GenerateKittyEnableSequence(flags emu.KeyProtocol) string {
-	return fmt.Sprintf("\x1b[>%d;1u", int(flags))
+	return fmt.Sprintf("\x1b[>%du", int(flags))
 }
 
 // GenerateKittyDisableSequence generates the escape sequence to disable Kitty protocol
