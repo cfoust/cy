e8d1e5e0bfd6954e532c276826e8443561689436
Caleb Foust <cfoust@sqweebloid.com>
2025-12-26 07:45:57 -0800

    feat: limit cos magnitude by time

--- pkg/anim/cos/module.go
+++ pkg/anim/cos/module.go
@@ -21,15 +21,14 @@ func (cos *Cos) Init(start image.Image) {
 }
 
 func (cos *Cos) Update(delta time.Duration) image.Image {
-	elapsed := delta.Seconds()
-	size := cos.out.Size()
-
-	var factor float64
-	var offset int
+	var (
+		elapsed = delta.Seconds()
+		size    = cos.out.Size()
+		factor  float64
+		offset  int
+	)
 	for col := 0; col < size.C; col++ {
-		factor = ((math.Cos((float64(col)/float64(size.C))*math.Pi*4+elapsed) + 1) / 8) * float64(
-			size.R,
-		)
+		factor = ((math.Cos((float64(col)/float64(size.C))*math.Pi*4+elapsed) + 1) / 8) * (elapsed / 20) * float64(size.R)
 		offset = int(factor)
 		for row := 0; row < size.R; row++ {
 			cos.out[(row+offset)%size.R][col] = cos.in[row][col]
