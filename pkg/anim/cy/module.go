5c46b253650fa14a57ffb9911071ce336426cea7
Caleb Foust <cfoust@sqweebloid.com>
2025-12-26 07:50:18 -0800

    fix: cy animation

--- pkg/anim/cy/module.go
+++ pkg/anim/cy/module.go
@@ -9,23 +9,27 @@ import (
 )
 
 type Cyform struct {
-	start image.Image
+	initial image.Image
+	current image.Image
 }
 
 var _ meta.Animation = (*Cyform)(nil)
 
 func (c *Cyform) Init(start image.Image) {
-	c.start = start
+	c.initial = start
+	c.current = start.Clone()
 }
 
 func (c *Cyform) Update(delta time.Duration) image.Image {
 	elapsed := delta.Seconds()
 	if elapsed > 1.0 {
-		return c.start
+		return c.current
 	}
 
-	end := 'a' + int32(elapsed*25)
-	mapping := make(map[rune]rune)
+	var (
+		end     = 'a' + int32(elapsed*25)
+		mapping = make(map[rune]rune)
+	)
 	for i := 'a'; i < end; i++ {
 		target := 'c'
 		if (i % 2) == 1 {
@@ -36,19 +40,19 @@ func (c *Cyform) Update(delta time.Duration) image.Image {
 		mapping[unicode.ToUpper(i)] = unicode.ToUpper(target)
 	}
 
-	size := c.start.Size()
-	for y := 0; y < size.R; y++ {
-		for x := 0; x < size.C; x++ {
-			current := c.start[y][x]
+	size := c.current.Size()
+	for row := 0; row < size.R; row++ {
+		for col := 0; col < size.C; col++ {
+			current := c.initial[row][col]
 
 			if mapped, ok := mapping[current.Char]; ok {
 				current.Char = mapped
-				current.FG = c.start[y][x].FG
+				current.FG = c.current[row][col].FG
 			}
 
-			c.start[y][x] = current
+			c.current[row][col] = current
 		}
 	}
 
-	return c.start
+	return c.current
 }
