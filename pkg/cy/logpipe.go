54a58fd321d22f0d6426df3a158baa0e4df4dd99
Caleb Foust <cfoust@sqweebloid.com>
2026-01-17 10:05:04 -0800

    feat: common janet pipe

--- pkg/cy/logpipe.go
+++ pkg/cy/logpipe.go
@@ -3,7 +3,6 @@ package cy
 import (
 	"context"
 	"io"
-	"os"
 
 	"github.com/cfoust/cy/pkg/janet"
 )
@@ -12,17 +11,7 @@ import (
 // (typically the logs pane). This allows print and eprint calls from user
 // Janet code to appear in cy's logs.
 type logPipe struct {
-	vm *janet.VM
-
-	stdoutR *os.File
-	stdoutW *os.File
-	stderrR *os.File
-	stderrW *os.File
-
-	outValue *janet.Value
-	errValue *janet.Value
-
-	dyns map[janet.Keyword]any
+	pipe *janet.Pipe
 }
 
 func newLogPipe(
@@ -30,58 +19,20 @@ func newLogPipe(
 	vm *janet.VM,
 	dest io.Writer,
 ) (*logPipe, error) {
-	stdoutR, stdoutW, err := os.Pipe()
+	pipe, err := vm.Pipe(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	stderrR, stderrW, err := os.Pipe()
-	if err != nil {
-		_ = stdoutR.Close()
-		_ = stdoutW.Close()
-		return nil, err
-	}
+	lp := &logPipe{pipe: pipe}
 
-	lp := &logPipe{
-		vm:      vm,
-		stdoutR: stdoutR,
-		stdoutW: stdoutW,
-		stderrR: stderrR,
-		stderrW: stderrW,
-	}
-
-	lp.outValue, err = vm.WrapFile(
-		ctx,
-		lp.stdoutW,
-		janet.FileFlagWrite,
-	)
-	if err != nil {
-		lp.Close()
-		return nil, err
-	}
-
-	lp.errValue, err = vm.WrapFile(
-		ctx,
-		lp.stderrW,
-		janet.FileFlagWrite,
-	)
-	if err != nil {
-		lp.Close()
-		return nil, err
-	}
-
-	lp.dyns = map[janet.Keyword]any{
-		janet.Keyword("out"): lp.outValue,
-		janet.Keyword("err"): lp.errValue,
-	}
-
-	go lp.stream(ctx, lp.stdoutR, dest)
-	go lp.stream(ctx, lp.stderrR, dest)
+	go lp.stream(ctx, pipe.Out, dest)
+	go lp.stream(ctx, pipe.Err, dest)
 
 	return lp, nil
 }
 
-func (lp *logPipe) stream(ctx context.Context, r *os.File, dest io.Writer) {
+func (lp *logPipe) stream(ctx context.Context, r io.Reader, dest io.Writer) {
 	buf := make([]byte, 4096)
 	for {
 		select {
@@ -104,7 +55,7 @@ func (lp *logPipe) stream(ctx context.Context, r *os.File, dest io.Writer) {
 // Dyns returns a map of dynamic bindings for :out and :err that can be passed
 // to Janet code execution.
 func (lp *logPipe) Dyns() map[janet.Keyword]any {
-	return lp.dyns
+	return lp.pipe.Dyns()
 }
 
 func (lp *logPipe) Close() {
@@ -112,23 +63,5 @@ func (lp *logPipe) Close() {
 		return
 	}
 
-	if lp.stdoutR != nil {
-		_ = lp.stdoutR.Close()
-	}
-	if lp.stdoutW != nil {
-		_ = lp.stdoutW.Close()
-	}
-	if lp.stderrR != nil {
-		_ = lp.stderrR.Close()
-	}
-	if lp.stderrW != nil {
-		_ = lp.stderrW.Close()
-	}
-
-	if lp.outValue != nil {
-		lp.outValue.Free()
-	}
-	if lp.errValue != nil {
-		lp.errValue.Free()
-	}
+	lp.pipe.Close()
 }
